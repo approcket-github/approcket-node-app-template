bd829c450b0c71c66c53d5368c0071abd7e398cd
Shehzore Mustajab <shehzore.mustajab@approcket.co>
2023-09-04 16:30:24 +0500

    Addition to dynamic DB Adapters

--- src/adapters/mongoAdapter.js
+++ src/adapters/mongoAdapter.js
@@ -1,5 +1,5 @@
 const DBAdapter = require('./dbAdapter');
-const User = require('../models/userModel');
+const User = require('../models/userModelMongoDB');
 
 class MongoAdapter extends DBAdapter {
   async createUser(username, password) {
